da5ab152592e70de02d1e5ef045cefd8fc566361
kolun4ik <11_ka@rambler.ru>
2019-06-14 21:59:37 +0300

    В раздел НОВСТИ добавлена постраничная навигация

--- news/views.py
+++ news/views.py
@@ -1,6 +1,6 @@
 import datetime
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, render_to_response
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from .models import ItemNews
 
 # Create your views here.
@@ -12,8 +12,23 @@ from .models import ItemNews
 def news_view(request, id_item):
     """представление отдельной новости"""
     if id_item == '':
-        items = ItemNews.objects.order_by('-creation_date')[:3]
-        return render(request, 'club_news.html', {'items': items})
+        items = ItemNews.objects.all().order_by('-creation_date')
+        paginator = Paginator(items,3)
+        page = request.GET.get('page')
+        try:
+            # Если страница существует, то выбираем ее
+            context = {
+                'items': paginator.page(page),
+            }
+        except PageNotAnInteger:
+            context = {
+                'items': paginator.page(1),
+            }
+        except EmptyPage:
+            context = {
+                'items': paginator.page(paginator.num_pages),
+            }
+        return render(request, 'club_news.html', context)
     else:
         item = ItemNews.objects.get(id=id_item)
         creation_date = datetime.datetime.strftime(item.creation_date, '%d.%m.%Y')
